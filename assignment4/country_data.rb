ed2e9f9d605f2673b7c297fad2653bd525f2adb2
saniyaa7 <114761701+saniyaa7@users.noreply.github.com>
2024-01-23 23:08:52 +0530

    Add country_data.rb

--- assignment4/country_data.rb
+++ assignment4/country_data.rb
@@ -8,7 +8,7 @@ module Data
     { name: 'Germany', gdp: 4_416_000_000_000, state_of_country: 'developed', army_strength: 5 },
     { name: 'Brazil', gdp: 1_608_000_000_000, state_of_country: 'developing', army_strength: 3 },
     { name: 'Australia', gdp: 1_488_000_000_000, state_of_country: 'developed', army_strength: 2 },
-    # ... other country data ...
+  
   ]
 end
 
