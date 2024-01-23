0cb344bab369d7cc686b6cd8c9bd811150b76bdb
saniyaa7 <114761701+saniyaa7@users.noreply.github.com>
2024-01-23 07:39:54 +0530

    Assignment-4 Add

---
+++ assignment4/country_data.rb
@@ -0,0 +1,14 @@
+# country_data.rb
+
+module Data
+  Country = [
+    { name: 'USA', gdp: 21_430_000_000_000, state_of_country: 'developed', army_strength: 8 },
+    { name: 'India', gdp: 2_877_000_000_000, state_of_country: 'developing', army_strength: 4 },
+    { name: 'China', gdp: 14_340_000_000_000, state_of_country: 'developing', army_strength: 9 },
+    { name: 'Germany', gdp: 4_416_000_000_000, state_of_country: 'developed', army_strength: 5 },
+    { name: 'Brazil', gdp: 1_608_000_000_000, state_of_country: 'developing', army_strength: 3 },
+    { name: 'Australia', gdp: 1_488_000_000_000, state_of_country: 'developed', army_strength: 2 },
+    # ... other country data ...
+  ]
+end
+
