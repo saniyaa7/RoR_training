f844adb61baab436e8dca8b127aed38bb2574e00
saniyaa7 <114761701+saniyaa7@users.noreply.github.com>
2024-02-04 12:57:25 +0530

    Delete transaction.rb

--- transaction.rb
+++
@@ -1,6 +0,0 @@
-class Transaction < ApplicationRecord
-    validates :transaction_name, :transaction_date, :transaction_amount, presence: true
-    validates :transaction_amount, numericality: true
-    has_many :reports
-    belongs_to :student
-end
